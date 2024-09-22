2f7437f3bf5080f931b2367f94e28f7969adcd28
Muhammad-Usman08 <usman2037966@gmail.com>
2024-09-22 21:31:21 +0500

    utils and package add

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:taskey_app/views/splash/splash_view.dart';
 
-void main() {}
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
---
+++ lib/utils/constants.dart
@@ -0,0 +1,1 @@
+const int themeColor = 0xff5451D6;
