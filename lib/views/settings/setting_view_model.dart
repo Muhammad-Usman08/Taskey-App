7008a94c3f2932daead11615e455481a446ed6f3
Asher Ayub <161920870+AsherAyub80@users.noreply.github.com>
2024-09-28 17:11:15 +0500

    add shared Prefrences for light dark theme

--- lib/views/settings/setting_view_model.dart
+++ lib/views/settings/setting_view_model.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingViewModel extends GetxController {
   var isPermissionEnabled = false.obs;
   var isDarkModeEnabled = false.obs;
   var isPushNotificationEnabled = false.obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    _loadThemePreference();
+  }
 
   void togglePermission() {
     isPermissionEnabled.value = !isPermissionEnabled.value;
@@ -12,6 +19,7 @@ class SettingViewModel extends GetxController {
 
   void toggleDarkMode() {
     isDarkModeEnabled.value = !isDarkModeEnabled.value;
+    _saveThemePreference(isDarkModeEnabled.value);
   }
 
   void togglePushNotification() {
@@ -20,5 +28,16 @@ class SettingViewModel extends GetxController {
 
   ThemeData get themeData {
     return isDarkModeEnabled.value ? ThemeData.dark() : ThemeData.light();
+  }
+
+  Future<void> _loadThemePreference() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    isDarkModeEnabled.value = prefs.getBool('dark_mode') ?? false;
+  }
+
+  // Save theme preference to SharedPreferences
+  Future<void> _saveThemePreference(bool isDarkMode) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('dark_mode', isDarkMode);
   }
 }
